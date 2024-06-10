86b962648e820bf85a7803f8a16e1bbc101a71b8
yasin <yasinmiapolash@gmail.com>
2024-06-10 15:23:57 +0600

    syncfusion_flutter_calendar

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -55,6 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               child: const Text('Read'),
             ),
+
             ElevatedButton(
               onPressed: () {
                 updateFriend();
